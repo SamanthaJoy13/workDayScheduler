82e040dd2e9fb0a0f06f6587ae1fc949650fde56
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-05-24 19:22:23 -0700

    updated all functions in js, removed classes in html

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,54 @@
-var header = $('<header>');
-var currentDate = dayjs().format('dddd MMMM DD, YYYY');
+//date display on header
+const header = $('<header>');
+const currentDate = dayjs().format('dddd MMMM DD, YYYY');
 header.text(currentDate);
 $('header').append(header);
 
-$(function () {
-var hour = dayjs().format('HH');
-var currentHour = $('#hour');
-
-if (hour > currentHour) {
-    currentHour.addClass('.past');
-} else if (hour < currentHour) {
-    currentHour.addClass('.future');
-} else { 
-    currentHour.addClass('.present');      
-}
-
+//timeblocks and save buttons
+const saveBtn = $('.saveBtn');
+$(saveBtn).on('click', function() {
+    const timeBlock = $(this).parent().attr('id');
+    const textArea = $(this).siblings('.description').val();
+    localStorage.setItem(timeBlock, textArea);
 });
 
-$('.saveBtn').on('click', function() {
-    var text = $(this).siblings('textarea').val();
-    var time = $(this).closest('.time-block').attr('id');
-    localStorage.setItem(time, text);
-}); 
+//timeblock color change
+function timeTracker() {
+    const currentHour = dayjs().hour();
+    $('.time-block').each(function() {
+        const blockHour = parseInt($(this).attr('id').split('-')[1]);
+        if (blockHour < currentHour) {
+            $(this).addClass('past');
+        } else if (blockHour === currentHour) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    });
+};
 
+timeTracker();
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//local storage
+const hour9 = $('#hour-9 .description');
+const hour10 = $('#hour-10 .description');
+const hour11 = $('#hour-11 .description');
+const hour12 = $('#hour-12 .description');
+const hour13 = $('#hour-13 .description');
+const hour14 = $('#hour-14 .description');
+const hour15 = $('#hour-15 .description');
+const hour16 = $('#hour-16 .description');
+const hour17 = $('#hour-17 .description');
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
